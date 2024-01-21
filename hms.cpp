47b92fa93b94acec59ccc283c1a50c205735cb0b
owualac <owualacharles@gmail.com>
2024-01-21 09:57:29 +0100

    saving hospital and patient data

---
+++ hms.cpp
@@ -0,0 +1,102 @@
+#include <bitset>/stdc++.h>
+#include <iostream>
+using namespace std;
+
+// store the hospital data
+
+class Hospital
+{
+public:
+    string H_name;
+    string locations;
+    int available_beds; // number
+    float ratings;
+    string contact;
+    string doctor_name;
+    int price;
+};
+
+// store patient data
+
+class Patient : public Hospital
+{
+public:
+    string P_name;
+    int P_id; // number
+};
+
+// Hospital data
+
+void PrintHospitalData(vector<Hospital> &hospitals)
+{
+    cout << "PRINT HOSPITAL DATA:"
+         << endl;
+
+    cout << "HospitalName"
+         << "Location"
+         << "Beds_Available"
+         << "Ratings"
+         << "Hospital_Contact"
+         << "Doctor_Name"
+         << "Price_Per_Bed \n";
+
+    for (int i = 0; i < 4 : i++)
+    {
+        cout << hospitals[i].H_name
+             << "     "
+             << " "
+             << hospitals[i].locations
+             << " "
+             << hospitals[i].available_beds
+             << " "
+             << hospitals[i].ratings
+             << " "
+             << hospitals[i].contact
+             << " "
+             << hospitals[i].doctor_name
+             << "     "
+             << " "
+             << hospitals[i].price
+             << " "
+             << endl;
+    }
+
+    cout << endl
+         << endl;
+}
+
+// print patient data in the hospital
+
+void print_patient(vector<Patient> &patients, vector<Hospitals> &hospitals)
+{
+    cout << "Print patients Data:"
+         << endl;
+    cout << "patient_Name  "
+         << "patient_id "
+         << "patient_contact "
+         << "alloted_hospital "
+         << "patient_expenditure  \n";
+
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "      "
+             << " "
+             << patients[i].P_name
+             << "    "
+             << " "
+             << patients[i].P_id
+             << "     "
+             << " "
+             << patients[i].contact
+             << "   "
+             << " "
+             << patients[i].H_name
+             << " "
+             << patients[i].price
+             << " "
+             << endl;
+    }
+
+    cout << endl
+         << endl;
+}
