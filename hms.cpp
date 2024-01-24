7a0145900e0b59b8da2b04b66022c9a0d824f2cc
owualac <owualacharles@gmail.com>
2024-01-24 09:25:51 +0100

    saving the final changes to HMS

--- hms.cpp
+++ hms.cpp
@@ -101,26 +101,186 @@ void print_patient(vector<Patient> &patients, vector<Hospitals> &hospitals)
          << endl;
 }
 
-//sort the hospital data by name
+// sort the hospital data by name
 
-bool name (Hospital& A, Hospital& B){
+bool name(Hospital &A, Hospital &B)
+{
     return A.H_name > B.H_name;
 }
 
-void sortHospitalByName (vector<Hospital> hospitals){
-    //sort the data
+void sortHospitalByName(vector<Hospital> hospitals)
+{
+    // sort the data
     sort(hospitals.begin(),
-    hospitals.end(), name
-    ):
+         hospitals.end(), name);
 
     cout << "Sort By Name:"
 
-        << endl
-        << endl;
-    
-        
+         << endl
+         << endl;
+
     PrintHospitalData(hospitals);
 }
-bool rating (Hospital& A, Hospital& B){
+bool rating(Hospital &A, Hospital &B)
+{
     return A.ratings > B.ratings;
 }
+
+// sort the ratings by numerating them
+
+void sortHospitalByRatings(vector<Hospital> hospitals)
+{
+    sort(hospitals.begin(), hospitals.end(), rating);
+
+    cout << "Sort By Rating:"
+         << endl
+         << endl;
+
+    PrintHospitalData(hospitals);
+}
+
+// comparator function to sort hospital data by Beds available
+
+bool Beds(Hospital &A, Hospital &B)
+{
+    return A.available_beds > B.available_beds;
+}
+
+// sort the data by beds available
+
+void sortHospitalByBedsAvailable(vector<Hospital> hospitals)
+{
+    sort(hospitals.begin(), hospitals.end(), Beds);
+
+    cout << "Sort By Available Beds"
+         << endl
+         << endl;
+
+    PrintHospitalData(hospitals);
+}
+
+// comparator function to sort hospital data by price
+
+bool Price(Hospital &A, Hospital &B)
+{
+    return A.price > B.price;
+}
+
+// sort data by price
+
+void sortHospitalByPrice(vector<Hospital> hospitals)
+{
+    sort(hospitals.begin(), hospitals.end(), Price);
+
+    cout << "Sort By Price"
+         << endl
+         << endl;
+
+    PrintHospitalData(hospitals);
+}
+
+// comparator function to sort hospital data by city
+
+void PrintHospitalByCity(
+    string city, vector<Hospital> hospitals)
+{
+    cout << "Print Hospitals By Name:"
+         << city << endl;
+
+    cout << "HospitalName "
+         << "Location "
+         << "Beds_Availabel "
+         << "Ratings "
+         << "Hospital_Contact "
+         << "Doctor_Name "
+         << "Price_Per_Bed \n";
+
+    for (int i = 0; i < 4 : i++)
+    {
+        if (hospitals[i].locations != city)
+            continue;
+        cout << "   "
+             << " "
+             << hospitals[i].locations
+             << " "
+             << hospitals[i].available_beds
+             << " "
+             << hospitals[i].ratings
+             << "  "
+             << hospitals[i].contact
+             << "   "
+             << hospitals[i].doctor_name
+             << "      "
+             << "    "
+             << hospitals[i].price
+             << " "
+             << endl;
+    }
+
+    cout << endl
+         << endl;
+}
+
+// funtion to implement Hospital Management system
+
+void HospitalManagement(
+    string patient_Name[], int patient_id[], string patient_contact[], int bookingCost[], string hospital_Name[], int booking[],
+    string location[], int beds[], float rating[], string hospital_Contact[],
+    string doctor_Name[], int prices[])
+
+    {
+
+        // store hospital and user data
+
+        vector<Hospital> hospitals;
+
+        // create objects for hospital and users
+
+        Hospital h;
+
+        for (size_t i = 0; i < 4; i++)
+        {
+            h.H_name = hospital_Name[i];
+            h.locations = location[i];
+            h.available_beds = beds[i];
+            h.ratings = rating[i];
+            h.contact = hospital_Contact[i];
+            h.doctor_name = doctor_Name[i];
+            h.price = prices[i];
+            hospitals.push_back(h);
+        }
+        
+        // store the patients data
+
+        vector<Patient> patients
+
+        Patient p;
+
+        for (size_t i = 0; i < 4; i++)
+        {
+            p.P_name = patient_Name[i];
+            p.P_id = patient_Id[i];
+            p.contact = patient_Contact[i];
+            p.price = bookingCost[i];
+            patients.push_back(p);
+        }
+
+        cout <<endl;
+
+        // call the various operations
+
+        PrintHospitalData(hospitals);
+        PrintPatientData(patients, hospitals);
+
+        sortHospitalByName(hospitals);
+        sortHospitalByRatings(hospitals);
+        sortHospitalByBedsAvailable(hospitals);
+        sortHospitalByPrice(hospitals);
+        PrintHospitalByCity("Lagos", hospitals);
+        
+        
+    }
+    
+
+    //Add Driver Code and make function call to test the code
+    
